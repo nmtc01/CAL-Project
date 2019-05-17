46627874dd28672c84a3159796d96d42919c50f0
pedrogalvao <36551740+pedrogalvao@users.noreply.github.com>
2019-05-17 18:57:15 +0100

    Added class system

---
+++ System.cpp
@@ -0,0 +1,16 @@
+/*
+ * System.cpp
+ *
+ *  Created on: 17 de mai de 2019
+ *      Author: Estudio
+ */
+
+include "System.h"
+using namespace std;
+
+Vertex<T> getSchool(){
+	return school;
+}
+Vertex<T> getGarage(){
+	return garage;
+}
---
+++ System.h
@@ -0,0 +1,39 @@
+/*
+ * System.h
+ *
+ *  Created on: 17 de mai de 2019
+ *      Author: Estudio
+ */
+
+#include <vector>
+using namespace std;
+
+template <class T> class Vertex;
+template <class T> class Graph;
+
+template <class T>
+class System {
+
+	Graph<T> map;
+	//vector<string> childrenAdresses;
+	Vertex<T> school;
+	Vertex<T> garage;
+	vector<Vertex<T> *> childrenVertices;    // vertex set
+	unsigned int numberOfBus;
+
+public:
+	void loadMap(string fileName);
+	Vertex<T> getSchool();
+	void setSchool(T address);
+	Vertex<T> getGarage();
+	void setGarage(T address);
+	vector<Vertex<T> *>	getChildrenVertices();
+	void insertAdress(T address);
+	void removeAdress(T address);
+	unsigned int getNumberOfBus();
+	void setNumberOfBus();
+
+	//void calculatePathsMatrix(); //calculate Matrix with shortest path for each pair of vertices
+	//void calculateBusPath();       //calculate shortest path passing by all the addresses
+
+};

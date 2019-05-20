47020d89c5ccd4e9a7993481a97345c6177b1613
pedrogalvao <36551740+pedrogalvao@users.noreply.github.com>
2019-05-20 10:45:38 +0100

    Update System.cpp

--- System.cpp
+++ System.cpp
@@ -14,3 +14,26 @@ Vertex<T> getSchool(){
 Vertex<T> getGarage(){
 	return garage;
 }
+
+void setSchool(T address){
+	school = map.findVertex(address);
+}
+
+void setGarage(T address){
+	garage = map.findVertex(address);
+}
+
+void insertAdress(T address){
+	childrenVertices.push_back(map.findVertex(address));
+}
+
+void removeAddress(T address){
+	vector<Vertex<T>*> newVector = {};
+	for (size_t = 0; i < childrenVertices.size(); i++){
+		if (childrenVertices[i]->getInfo() != address){
+			newVector.push_back(childrenVertices[i]);
+		}
+		else break;
+	}
+	
+}

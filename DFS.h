2f622bdb77080039d0f5f5a7f3047355bb0813af
Nuno Cardoso <ns.cardoso@hotmail.com>
2019-05-22 18:51:58 +0100

    Added DFS.h

--- DFS.h
+++ DFS.h
@@ -5,6 +5,17 @@
  *      Author: Nuno Cardoso
  */
 
+#pragma once
 
+#include <unordered_set>
+#include "Vertex.h"
 
+using namespace std;
 
+class DFS {
+	VertexHashTable possibleVertexes;
+
+public:
+	DFS();
+
+};

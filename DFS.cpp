fdccb02fb4686aba4ea794853a28f3029181e5f6
Nuno Cardoso <ns.cardoso@hotmail.com>
2019-05-22 19:02:25 +0100

    Added functions structure

---
+++ DFS.cpp
@@ -0,0 +1,23 @@
+/*
+ * DFS.cpp
+ *
+ *  Created on: 22/05/2019
+ *      Author: Nuno Cardoso
+ */
+
+#include "DFS.h"
+
+DFS::DFS(Graph &graph) {
+	this->graph = graph;
+}
+
+void perform(Vertex origin) {
+
+}
+
+void visitNodes(Vertex origin) {
+
+}
+
+
+

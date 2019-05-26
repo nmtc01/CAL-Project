fdd33a903ff89333a938a6d8c3a770a3500a7a8c
Nuno Cardoso <ns.cardoso@hotmail.com>
2019-05-26 22:52:47 +0100

    Initial conditions made to branch and bound, need to finish perform

--- BranchAndBound.h
+++ BranchAndBound.h
@@ -12,21 +12,23 @@
 typedef vector<vector<double>> matrix;
 
 class BranchAndBound {
-
+private:
 	Graph graph;
 	VertexHashTable visitedVertices;
 	matrix weightMatrix;
-	double upperBound;
+	double upperBound = INF;
+	double lowerBound = 0;
 
 public:
-	BranchAndBound(Graph graph, double upperBound);
+	BranchAndBound(Graph graph, double upperBound, double lowerBound);
 	void initializeMatrix();
-	double findMinLine();
-	double findMinCol();
-	void reduceMatrixLine();
-	void reduceMatrixCol();
+	double findMinLine(matrix weights, int line);
+	double findMinCol(matrix weights, int col);
+	void reduceMatrixLine(matrix &weights, int line);
+	void reduceMatrixCol(matrix &weights, int col);
+	void reduceMatrix(matrix &weights);
+	void visitVertex();
 	VertexHashTable perform(const unsigned &originId);
-
 };
 
 

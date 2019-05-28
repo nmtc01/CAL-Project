0312534372cc0b20778eb3fdf95dab9e9a638961
Miguel Pinto <pt.miguel99@gmail.com>
2019-05-28 18:09:12 +0100

    Finished creating StudentHouse

--- StudentHouse.h
+++ StudentHouse.h
@@ -4,3 +4,24 @@
  */
 
 #pragma once
+
+class StudentHouse {
+	unsigned houseId;
+	unsigned schoolId;
+	unsigned numberOfStudents;
+
+public:
+	//Constructors
+	StudentHouse();
+	StudentHouse(unsigned houseId, unsigned schoolId, unsigned numberOfStudents);
+
+	//Get Methods
+	unsigned getHouseId() const;
+	unsigned getSchoolId() const;
+	unsigned getNumberOfStudents() const;
+
+	//Set number of students
+	void setHouseId(unsigned id);
+	void setSchoolId(unsigned id);
+	void setNumberOfStudents(unsigned number);
+};
